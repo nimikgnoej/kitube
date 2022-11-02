783e76b23178698fe3e71197c0c5a099b110e81a
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2022-11-02 16:51:15 +0900

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("See video");
-export const edit = (req, res) => res.send("Edit Video");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Uploads Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
