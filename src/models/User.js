7e6ce4b42c84885aff3439606abacf13da1feef1
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2022-11-15 17:13:17 +0900

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,11 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
+    avatarUrl: String,
+    socialOnly: { type: Boolean, default: false },
+    githubId: { type: Number },
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String },
     name: { type: String, required: true },
     location: String,
 });
