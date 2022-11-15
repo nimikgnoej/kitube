0273c1c2ff541bf4a85511016eb380e05377559c
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2022-11-15 17:13:09 +0900

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
 import { trusted } from "mongoose";
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
+import { token } from "morgan";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "회원가입" })
 export const postJoin = async (req, res) => {
@@ -34,7 +36,7 @@ export const postLogin = async (req, res) => {
     //계정이 있는지 확인하기
 
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username, socialOnly: false });
     if (!user) {
         return res.status(400).render("login", { pageTitle: "로그인", errorMessage: "해당 이름을 가진 사용자가 존재하지 않아요 😭" });
     }
@@ -49,7 +51,81 @@ export const postLogin = async (req, res) => {
     return res.redirect("/");
 }
 
+export const startGithubLogin = (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/authorize";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        allow_signup: false,
+        scope: "read:user user:email",
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+}
+
+export const finishGithubLogin = async (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code,
+    };
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await (
+        await fetch(finalUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+            },
+        })
+    ).json();
+    if ("access_token" in tokenRequest) {
+        const { access_token } = tokenRequest;
+        const apiUrl = "https://api.github.com"
+        const userData = await (
+            await fetch(`${apiUrl}/user`, {
+                headers: {
+                    Authorization: `token ${access_token}`,
+                },
+            })
+        ).json();
+        const emailData = await (
+            await fetch(`${apiUrl}/user/emails`, {
+                headers: {
+                    Authorization: `token ${access_token}`,
+                },
+            })
+        ).json();
+        const emailObj = emailData.find(
+            (email) => email.primary === true && email.verified === true
+        );
+        if (!emailObj) {
+            return res.redirect("/login");
+        }
+        let user = await User.findOne({ email: emailObj.email });
+        if (!user) {
+            user = await User.create({
+                avatarUrl: userData.avatar_url,
+                name: userData.name,
+                username: userData.login,
+                email: emailObj.email,
+                password: "",
+                socialOnly: true,
+                location: userData.location,
+            });
+        }
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
+    } else {
+        return res.redirect("/login");
+    }
+}
+
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
 export const see = (req, res) => res.send("See User");
-export const logout = (req, res) => res.send("Log Out");
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
