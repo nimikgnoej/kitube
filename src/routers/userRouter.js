7523cd5784d1881ba429186e23fe90cb06453ecd
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2022-11-21 17:21:19 +0900

    Update userRouter.js

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { postEdit, getEdit, see, logout, startGithubLogin, finishGithubLogin, startKakaoLogin, finishKakaoLogin } from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
+import { postEdit, getEdit, see, logout, startGithubLogin, finishGithubLogin, startKakaoLogin, finishKakaoLogin, getChangePassword, postChangePassword } from "../controllers/userController";
+import { avatarUpload, protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUpload.single("avatar"), postEdit);
+userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/kakao/start", publicOnlyMiddleware, startKakaoLogin);
