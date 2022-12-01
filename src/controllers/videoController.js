559146e19fc0115a1d0344d36ca1bedfc5b1fe43
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2022-12-01 18:06:06 +0900

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 import User from "../models/User";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" }).populate("owner");
   return res.render("home", { pageTitle: "기튜브", videos });
 };
 export const watch = async (req, res) => {
