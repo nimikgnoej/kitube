92f96c458b272a21e8ff8c8c1f110890c5ca317b
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2022-12-02 16:23:00 +0900

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -106,7 +106,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(keyword, "i"),
       },
-    });
+    }).populate("owner");
   };
   return res.render("search", { pageTitle: "영상 검색", videos });
 }
