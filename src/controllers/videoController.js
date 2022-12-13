3c27550b5eb81eccb7e6d7bfa0b78c99526e157d
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2022-12-13 17:30:17 +0900

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import Video from "../models/Video";
 import User from "../models/User";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   const videos = await Video.find({}).sort({ createdAt: "desc" }).populate("owner");
@@ -123,4 +124,24 @@ export const registerView = async (req, res) => {
   video.meta.views += 1;
   await video.save();
   return res.sendStatus(200);
+}
+
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const comment = await Comment.create({
+    text: text,
+    owner: user._id,
+    video: id,
+  });
+
+  return res.sendStatus(201);
 }
