b8f39e8e1ec81c3acebf3add9a330f17db698a00
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2022-12-14 17:55:49 +0900

    Update commentSection.js

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,9 +1,26 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
+const deleteComment = document.getElementById("deleteComment");
+const commentTar = document.getElementById("commentList");
 
+const addComment = (text, id) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.dataset.id = id;
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span");
+    span.innerText = ` ${text}`;
+    const span2 = document.createElement("span");
+    span2.innerText = " ❌";
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    newComment.appendChild(span2);
+    videoComments.prepend(newComment);
+}
 
-
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
     const text = textarea.value;
@@ -11,7 +28,7 @@ const handleSubmit = (event) => {
     if (text === "") {
         return;
     }
-    fetch(`/api/videos/${videoId}/comment`, {
+    const response = await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -20,10 +37,22 @@ const handleSubmit = (event) => {
             text,
         }),
     });
-    textarea.value = "";
+    if (response.status == 201) {
+        textarea.value = "";
+        const { newCommentId } = await response.json();
+        addComment(text, newCommentId);
+    }
 };
+
+const handleDeleteComment = async () => {
+    const commentId = commentTar.dataset.id;
+    await fetch(`/api/comments/${commentId}`, {
+        method: "DELETE",
+    });
+}
 
 
 if (form) {
     form.addEventListener("submit", handleSubmit);
 }
+deleteComment.addEventListener("click", handleDeleteComment);
